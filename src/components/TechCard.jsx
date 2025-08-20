a83c18d91e46245239b8fdf8d689f3928d5db1b9
Ramiro Sánchez Torres <ujhik@usal.es>
2025-08-20 18:18:02 +0200

    feature(fullTree): All tree elements added and working
    All tech tree components are added and visualized correctly

--- src/components/TechCard.jsx
+++ src/components/TechCard.jsx
@@ -4,30 +4,38 @@ import { Handle } from '@xyflow/react';
 import styles from './TechCard.module.css';
 
 const TechCard = (props) => {
-
-  const renderList = (items, dict, containerClass, itemClass) => (
-    <div className={containerClass}>
-      {items?.map((item, index) => (
-        <div key={index} className={itemClass}>
-          {dict[item]?.name}
-        </div>
-      ))}
+  const renderList = (items, dict, containerClass, itemClass, imageFolderPath) => (
+    <div className={`${containerClass} ${styles.unlockedBlock}`}>
+      {items?.map((item, index) => {
+        const name = dict[item]?.name; 
+        
+        return (
+          <div key={index} className={itemClass}>
+            <img src={`/${imageFolderPath}/${item}.png`} />
+            <div>
+              {name}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 
   return (
     <div className={styles.techCard}>
       <h3>{props.data.name}</h3>
-      {props.data.prerequisites?.[1] && (
-        <>({props.data.prerequisites[1]})</>
-      )}
-      {renderList(props.data.units, props.data.unitsDict, styles.units, styles.unit)}
-      {renderList(props.data.buildings, props.data.buildingsDict, styles.buildings, styles.building)}
-      {renderList(props.data.wonders, props.data.wondersDict, styles.wonders, styles.wonder)}
-      {renderList(props.data.terrains, props.data.terrainsDict, styles.terrains, styles.terrain)}
-      {renderList(props.data.spaceship_parts, props.data.spaceshipPartsDict, styles.spaceship_parts, styles.spaceship_part)}
-      <Handle type="source" position="right" />
-      <Handle type="target" position="left" />
+      <div className={styles.content}>
+        {props.data.prerequisites?.[1] && (
+          <>({props.data.prerequisites[1]})</>
+        )}
+        {renderList(props.data.units, props.data.unitsDict, styles.units, styles.unit, "units")}
+        {renderList(props.data.buildings, props.data.buildingsDict, styles.buildings, styles.building, "buildings")}
+        {renderList(props.data.wonders, props.data.wondersDict, styles.wonders, styles.wonder, "wonders")}
+        {renderList(props.data.terrains, props.data.terrainsDict, styles.terrains, styles.terrain, "terrains")}
+        {renderList(props.data.spaceship_parts, props.data.spaceshipPartsDict, styles.spaceship_parts, styles.spaceship_part, "spaceship_parts")}
+        <Handle type="source" position="right" />
+        <Handle type="target" position="left" />
+      </div>
     </div>
   );
 };
